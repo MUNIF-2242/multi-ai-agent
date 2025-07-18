dba57a8be830993737b529ff3e670f4370554a40
MUNIF-2242 <munif.shellbeehaken@gmail.com>
2025-07-18 12:00:03 +0600

    change model

--- services/model_loader.py
+++ services/model_loader.py
@@ -7,7 +7,7 @@ def get_bedrock_model():
     aws_region = os.getenv('AWS_REGION', 'us-east-1')
     
     return BedrockModel(
-        model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+        model_id="us.amazon.nova-micro-v1:0",
         temperature=0.7,
         region_name=aws_region
     )
